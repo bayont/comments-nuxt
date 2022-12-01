e76cf6029ed39a29b8a07558beb247659ef50bf3
bayont <f.fetter02@gmail.com>
2022-12-01 23:13:25 +0100

    feat: add avatar placeholder

---
+++ components/Avatar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img :src="path" alt="Avatar" />
+  </div>
+</template>
+<script>
+export default {
+  props: ['path']
+};
+</script>
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+</style>
